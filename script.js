31361c02b5b5c8a66d4c3190f7c2bc4b01611a81
Joseph Murray <josephl.murrayjr@gmail.com>
2022-03-31 18:24:17 -0400

    collapse questions into an array

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Arrays for special characters, lowercase letters, uppercase letters, and numbers
-// const specialChars = ;
-
-// const numberChars = ;
-
-// const uppercase = ;
-
-// const lowercase = ;
-
 const possibleSet = [
   ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"],
   ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
@@ -14,28 +5,23 @@ const possibleSet = [
   ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 ]
 
+const questions = ["Include Special Characters?", "Include Numbers?", "Include Uppercase Letters?", "Include Lowercase Letters?"]
 
-//initial prompt with checks for validiy (is number, >=8, <=128). assign value to var
 const generatePassword =() => {
   let userChoice = prompt("Enter a numeric value between 8 and 128 for your password:");
   let choice = parseInt(userChoice);
+  let possibleCharacters = [];
   let password = "";
-
+  
+  //initial prompt with checks for validiy (is number, >=8, <=128). assign value to var
   if ((isNaN(choice)) || (choice < 8) || (choice > 128)) {
     window.alert("That is not a valid selection. Please try again.");
     return password;
   }
 
   //prompts with concatenation with a final array length check that returns to beginning if lenth=0
-  let possibleCharacters = [];
-  let specialChars = confirm("Include Special Characters?")
-  let numberChars = confirm("Include Numbers?")
-  let uppercase = confirm("Include Uppercase Letters?")
-  let lowercase = confirm("Include Lowercase Letters?")
-  let userResponse = [specialChars, numberChars, uppercase, lowercase]
-
-  for (i=0;i < 4; i++) {
-    if (userResponse[i]) {
+  for (i=0; i < questions.length; i++) {
+    if (confirm(questions[i])) {
       possibleCharacters = possibleCharacters.concat(possibleSet[i]);
     }
   }
